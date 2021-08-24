6675f6c773e611b42dbb37448a0cbe53b2ab30af
katiejlee <ktlee6455@gmail.com>
2021-08-24 14:49:19 -0400

    added arrays and changes to the dice in each roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,31 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //WHO          WHAT    VALUE
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        //Challenge: Setting Alpha Value - diceImageView1.alpha = 0.5
         
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        /*  Strings use double quotations
-         
-            To add code in a print statement/String,
-            use \() and whatever is in the parens will
-            be calculated
-        */
-        print("Button got tapped.")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
     }
     
 }
