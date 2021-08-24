3eff30dec28d6cb66ec168a52834768cf3af3375
katiejlee <ktlee6455@gmail.com>
2021-08-24 14:12:56 -0400

    changed images to different ones and added some functionality to the roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //WHO          WHAT    VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        //Challenge: Setting Alpha Value - diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        /*  Strings use double quotations
+         
+            To add code in a print statement/String,
+            use \() and whatever is in the parens will
+            be calculated
+        */
+        print("Button got tapped.")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
